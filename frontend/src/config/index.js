79cd3ed09f9a57c547a3373efdc308738517e919
Sebastian906 <elsebas1912@gmail.com>
2025-08-20 11:48:06 -0500

    Descarga y configuración de componentes para formularios dentro de la página

---
+++ frontend/src/config/index.js
@@ -0,0 +1,23 @@
+export const registerFormControls = [
+    {
+        name: 'userName',
+        label: 'User name',
+        placeholder: 'Ingrese su nombre de usuario',
+        componentType: 'input',
+        type: 'text',
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        placeholder: 'Ingrese su correo',
+        componentType: 'input',
+        type: 'email',
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Ingrese su contraseña',
+        componentType: 'input',
+        type: 'password',
+    },
+]
