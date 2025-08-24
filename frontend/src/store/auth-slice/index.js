7a9bbb9032ee4a959c314e83f3fe21814cbac53e
Sebastian906 <elsebas1912@gmail.com>
2025-08-24 16:51:54 -0500

    Agregar casos de autenticación para el inicio de sesión

--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from "axios"
 
@@ -18,23 +19,41 @@ export const registrarUsuario = createAsyncThunk('/autorizacion/registro',
     }
 )
 
+export const loginUsuario = createAsyncThunk('/autorizacion/iniciar-sesion',
+    async (formData) => {
+        const response = await axios.post(
+            'http://localhost:5000/api/autorizacion/iniciar-sesion',
+            formData, {
+            withCredentials: true
+        });
+        return response.data
+    }
+)
+
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        // eslint-disable-next-line no-unused-vars
         setUser: (state, action) => {},
     },
     extraReducers: (builder)=> {
         builder.addCase(registrarUsuario.pending, (state) => {
             state.estaCargando = true
-        // eslint-disable-next-line no-unused-vars
         }).addCase(registrarUsuario.fulfilled, (state, action) => {
             state.estaCargando = false;
             state.usuario = null;
             state.estaAutenticado = false;
-        // eslint-disable-next-line no-unused-vars
         }).addCase(registrarUsuario.rejected, (state, action) => {
+            state.estaCargando = false;
+            state.usuario = null;
+            state.estaAutenticado = false;
+        }).addCase(loginUsuario.pending, (state) => {
+            state.estaCargando = true
+        }).addCase(loginUsuario.fulfilled, (state, action) => {
+            state.estaCargando = false;
+            state.usuario = action.payload;
+            state.estaAutenticado = true;
+        }).addCase(loginUsuario.rejected, (state, action) => {
             state.estaCargando = false;
             state.usuario = null;
             state.estaAutenticado = false;
