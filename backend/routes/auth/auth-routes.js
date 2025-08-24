caac32adbc92af2ef4ab1f7048b8244129b6d1a4
Sebastian906 <elsebas1912@gmail.com>
2025-08-24 16:46:44 -0500

    Creación de ruta para exportar API de inicio de sesión

--- backend/routes/auth/auth-routes.js
+++ backend/routes/auth/auth-routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { registroUsuario } = require('../../controllers/auth/auth-controller')
+const { registroUsuario, loginUsuario } = require('../../controllers/auth/auth-controller')
 
 const router = express.Router()
 
-router.post('/registro', registroUsuario)
+router.post('/registro', registroUsuario);
+router.post('/login', loginUsuario);
 
 module.exports = router;
