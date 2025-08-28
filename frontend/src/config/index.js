73722f9d5e7d1e840e6257f9e9a04573091b8581
Sebastian906 <elsebas1912@gmail.com>
2025-08-28 16:57:45 -0500

    Configuración de componente para añadir productos como administrador

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -38,3 +38,65 @@ export const loginFormControls = [
         type: 'password',
     },
 ]
+
+export const addProductFormElements = [
+    {
+        label: "Titulo",
+        name: "titulo",
+        componentType: "input",
+        type: "text",
+        placeholder: "Ingrese el nombre del producto",
+    },
+    {
+        label: "Descripcion",
+        name: "descripcion",
+        componentType: "textarea",
+        placeholder: "Ingrese la descripción del producto",
+    },
+    {
+        label: "Categoria",
+        name: "categoria",
+        componentType: "select",
+        options: [
+            { id: "hombre", label: "Hombre" },
+            { id: "mujer", label: "Mujer" },
+            { id: "niños", label: "Niños" },
+            { id: "accesorios", label: "Accesorios" },
+            { id: "calzado", label: "Calzado" },
+        ],
+    },
+    {
+        label: "Marca",
+        name: "marca",
+        componentType: "select",
+        options: [
+            { id: "nike", label: "Nike" },
+            { id: "adidas", label: "Adidas" },
+            { id: "puma", label: "Puma" },
+            { id: "levi", label: "Levi's" },
+            { id: "zara", label: "Zara" },
+            { id: "h&m", label: "H&M" },
+        ],
+    },
+    {
+        label: "Precio",
+        name: "precio",
+        componentType: "input",
+        type: "number",
+        placeholder: "Ingrese el precio del producto",
+    },
+    {
+        label: "Precio de Venta",
+        name: "precioVenta",
+        componentType: "input",
+        type: "number",
+        placeholder: "Ingrese el precio de venta (opcional)",
+    },
+    {
+        label: "Existencia Total",
+        name: "existenciaTotal",
+        componentType: "input",
+        type: "number",
+        placeholder: "Ingrese unidades del producto",
+    },
+];
--- frontend/src/pages/admin/products.jsx
+++ frontend/src/pages/admin/products.jsx
@@ -1,8 +1,60 @@
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { Fragment, useState } from "react";
+
+const initialFormData = {
+    imagen: null,
+    titulo: '',
+    descripcion: '',
+    categoria: '',
+    marca: '',
+    precio: "",
+    precioVenta: "",
+    existenciaTotal: "",
+}
+
 function AdminProducts() {
-    return ( 
-        <div>
-            Admin Products
-        </div>
+
+    const [openCreateProductsDialog, setOpenCreateProductsDialog] = useState(false);
+
+    const [formData, setFormData] = useState(initialFormData);
+
+    function onSubmit() {
+
+    }
+
+    return (
+        <Fragment>
+            <div className="mb-5 w-full flex justify-end">
+                <Button onClick={() => setOpenCreateProductsDialog(true)}>
+                    Agregar Nuevo Producto
+                </Button>
+            </div>
+            <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+            <Sheet open={openCreateProductsDialog} onOpenChange={() => {
+                setOpenCreateProductsDialog(false);
+            }}
+            >
+                <SheetContent side="right" className="overflow-auto [&>button]:text-white [&>button]:hover:text-gray-300">
+                    <SheetHeader>
+                        <SheetTitle>
+                            Agregar Nuevo Producto
+                        </SheetTitle>
+                    </SheetHeader>
+                    <div className="py-6">
+                        <CommonForm
+                            onSubmit={onSubmit}
+                            formData={formData}
+                            setFormData={setFormData}
+                            buttonText='Agregar'
+                            formControls={addProductFormElements}
+                        />
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </Fragment>
     );
 }
 
