9cba44398b94544072dbe52d0eb3eb9976402047
Sebastian906 <elsebas1912@gmail.com>
2025-08-31 21:26:58 -0500

    Adición de rutas de las APIs correspondientes del controlador de productos

--- backend/routes/admin/products-routes.js
+++ backend/routes/admin/products-routes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
-const { manejarCargaImagen } = require('../../controllers/admin/products-controller')
+const { 
+    manejarCargaImagen,
+    agregarProducto, 
+    listarProductos,
+    editarProducto, 
+    borrarProducto
+} = require('../../controllers/admin/products-controller')
 
 const { upload } = require('../../helpers/cloudinary')
 
 const router = express.Router();
 
 router.post('/subir-imagen', upload.single('archivo'), manejarCargaImagen);
+router.post('/agregar', agregarProducto)
+router.get('/lista', listarProductos)
+router.put('/editar/:id', editarProducto)
+router.delete('/borrar/:id', borrarProducto)
 
 module.exports = router;
