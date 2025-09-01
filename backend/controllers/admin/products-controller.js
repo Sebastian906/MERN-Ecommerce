3de2681cc59cbab159335942a1c779eb69e4bde1
Sebastian906 <elsebas1912@gmail.com>
2025-08-31 20:18:24 -0500

    Creación de API para editar todos los productos

--- backend/controllers/admin/products-controller.js
+++ backend/controllers/admin/products-controller.js
@@ -76,6 +76,37 @@ const listarProductos = async (req, res) => {
 // editar un producto
 const editarProducto = async (req, res) => {
     try {
+        const { id } = req.params;
+        const {
+            imagen,
+            titulo,
+            descripcion,
+            categoria,
+            marca,
+            precio,
+            precioVenta,
+            existenciaTotal,
+        } = req.body;
+
+        const buscarProducto = await Producto.findById(id);
+        if (!buscarProducto) return res.status(404).json({
+            success: false,
+            message: 'Producto no encontrado',
+        });
+        buscarProducto.titulo = titulo || buscarProducto.titulo
+        buscarProducto.descripcion = descripcion || buscarProducto.descripcion
+        buscarProducto.categoria = categoria || buscarProducto.categoria
+        buscarProducto.marca = marca || buscarProducto.marca
+        buscarProducto.precio = precio || buscarProducto.precio
+        buscarProducto.precioVenta = precioVenta || buscarProducto.precioVenta
+        buscarProducto.existenciaTotal = existenciaTotal || buscarProducto.existenciaTotal
+        buscarProducto.imagen = imagen || buscarProducto.imagen
+
+        await buscarProducto.save();
+        res.status(200).json({
+            success: true,
+            data: buscarProducto,
+        });
 
     } catch (e) {
         console.log(e);
