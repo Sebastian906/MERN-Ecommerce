8e03b96e69ea28eb7b410cdf302cf096c87d5731
Sebastian906 <elsebas1912@gmail.com>
2025-09-03 21:07:33 -0500

    Cambios en los estilos de las cards y comparación entre precio normal y precio de venta

--- frontend/src/components/admin/product-tile.jsx
+++ frontend/src/components/admin/product-tile.jsx
@@ -11,22 +11,26 @@ function AdminProductTile({ producto }) {
                     className="w-full h-[300px] object-cover"
                 />
             </div>
-            <CardContent className="p-3">
-                <h2 className="text-lg font-bold mb-1">
+            <CardContent className="px-6 pt-0.5 pb-1">
+                <h2 className="text-lg font-bold mb-2">
                     {producto?.titulo}
                 </h2>
-                <div className="flex justify-between items-center mb-1">
+                <div className="flex justify-between items-center mb-2">
                     <span className={`${producto?.precioVenta > 0 ? 'line-through' : ''} text-base font-semibold text-primary`}>
                         ${producto?.precio}
                     </span>
-                    <span className="text-base font-bold">
-                        ${producto?.precioVenta}
-                    </span>
+                    {
+                        producto?.precioVenta > 0 ? (
+                            <span className="text-base font-bold">
+                                ${producto?.precioVenta}
+                            </span>
+                        ) : null
+                    }
                 </div>
             </CardContent>
-            <CardFooter className="flex justify-center gap-3 items-center px-3 pb-3 pt-0">
-                <Button>Editar</Button>
-                <Button>Borrar</Button>
+            <CardFooter className="flex justify-center gap-8 items-center px-1 pb-5 pt-0.5">
+                <Button size="sm" className="!bg-teal-600 hover:!bg-teal-700 !text-neutral-100 !px-5">Editar</Button>
+                <Button size="sm" className="!bg-rose-600 hover:!bg-rose-700 !text-neutral-100 !px-5">Borrar</Button>
             </CardFooter>
         </Card>
     );
