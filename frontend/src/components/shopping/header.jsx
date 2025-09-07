c8c933106db35d5f5b890eb17f953772a6659f66
Sebastian906 <elsebas1912@gmail.com>
2025-09-07 16:46:19 -0500

    Configuración inicial de la cabecera para la lista de productos en la interfaz principal

--- frontend/src/components/shopping/header.jsx
+++ frontend/src/components/shopping/header.jsx
@@ -1,8 +1,40 @@
+import { LuChartPie, LuMenu, LuStore } from "react-icons/lu";
+import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
+
 function ShoppingHeader() {
+
+    const { estaAutenticado } = useSelector(state => state.auth);
+
+
     return ( 
-        <div>
-            Shopping Header
-        </div>
+        <header className="sticky top-0 z-40 w-full border-b bg-background">
+            <div className="flex h-16 items-center justify-between px-4 md:px-6">
+                <Link to='/tienda/inicio' className="flex items-center gap-2">
+                    <LuStore className="h-6 w-6 text-black" />
+                    <span className="font-bold text-2xl text-black">OutfitOn</span>
+                </Link>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon" className="lg:hidden">
+                            <LuMenu className="h-6 w-6 bg-slate-900 hover:bg-slate-700 text-white" />
+                            <span className="sr-only">Menú alterno</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left" className="w-full max-w-xs">
+
+                    </SheetContent>
+                </Sheet>
+                <div className="hidden lg:block">
+
+                </div>
+                {
+                    estaAutenticado ? <div></div> : null
+                }
+            </div>
+        </header>
     );
 }
 
