5948c1a7400fdb47f1737e4e3d0c5ee8c20b9ede
Sebastian906 <elsebas1912@gmail.com>
2025-09-12 09:15:11 -0500

    Configuración en la API de productos en la tienda para ordenarlos de acuerdo a los filtros seleccionados

--- backend/controllers/shop/products-controller.js
+++ backend/controllers/shop/products-controller.js
@@ -1,18 +1,52 @@
 const Product = require("../../models/Product");
 
-const obtenerProductosFiltrados = async(req, res) => {
+const obtenerProductosFiltrados = async (req, res) => {
     try {
-        const productos = await Product.find({})
+        // Mapear los nombres de los filtros del frontend a los del modelo
+        const {
+            Categoria = [],
+            Marca = [],
+            ordenarPor = "precio-menoramayor"
+        } = req.query;
+        let filtros = {};
+
+        if (Categoria.length) {
+            filtros.categoria = { $in: Categoria.split(',') };
+        }
+        if (Marca.length) {
+            filtros.marca = { $in: Marca.split(',') };
+        }
+
+        let ordenar = {};
+        switch (ordenarPor) {
+            case "precio-menoramayor":
+                ordenar.precio = 1;
+                break;
+            case "precio-mayoramenor":
+                ordenar.precio = -1;
+                break;
+            case "titulo-az":
+                ordenar.titulo = 1;
+                break;
+            case "titulo-za":
+                ordenar.titulo = -1;
+                break;
+            default:
+                ordenar.precio = 1;
+                break;
+        }
+
+        const productos = await Product.find(filtros).sort(ordenar);
         res.status(200).json({
             success: true,
             data: productos
-        })
+        });
     } catch (e) {
-        console.log(error);
+        console.log(e);
         res.status(500).json({
             success: false,
             message: 'Ha ocurrido un error'
-        })
+        });
     }
 }
 
