a45185beb81313410a6d8ef809fcfedfa1b9d69b
Sebastian906 <elsebas1912@gmail.com>
2025-09-18 21:11:01 -0500

    Se incluyen imagenes de assets

--- frontend/src/pages/shopping/home.jsx
+++ frontend/src/pages/shopping/home.jsx
@@ -1,7 +1,9 @@
 function ShoppingHome() {
     return ( 
-        <div className="bg-fuchsia-100">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem soluta dolorum qui. Quod, reiciendis! Expedita praesentium illum cupiditate voluptates molestiae deleniti quis sapiente, repudiandae nam excepturi odit ea, minus numquam asperiores similique culpa veniam veritatis vel non magni id rem possimus officiis? Laudantium dolores et esse minus itaque cum eligendi quae velit libero rem. In fuga error voluptate maiores iste ut minus non aut velit voluptates earum, ullam cum doloribus mollitia recusandae ab quos praesentium tempora ipsam aspernatur? Harum eaque placeat non, officiis amet voluptates odio error architecto debitis praesentium, facilis dolor. Non illo soluta omnis adipisci nesciunt quam accusamus.
+        <div className="flex flex-col min-h-screen bg-pink-100">
+            <div className="relative w-full h-[600px] overflow-hidden">
+
+            </div>
         </div>
     );
 }
