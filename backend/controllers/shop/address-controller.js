c8a59ccc8bb6b8233706b8baa5493955397d80ac
Sebastian906 <elsebas1912@gmail.com>
2025-09-22 15:14:57 -0500

    Creación de API para crear una nueva cuenta

--- backend/controllers/shop/address-controller.js
+++ backend/controllers/shop/address-controller.js
@@ -1,6 +1,25 @@
+const Cuenta = require('../../models/Address')
+
 const agregarCuenta = async(req, res) => {
     try {
-        
+        const { usuarioId, direccion, ciudad, codigopin, telefono, notas } = req.body;
+        if (!usuarioId || !direccion || !ciudad || !codigopin || !telefono || !notas) {
+            return res.status(400).json({
+                success: false,
+                message: 'Datos inválidos!'
+            })
+        }
+        const nuevaCuenta = new Cuenta({
+            usuarioId, direccion, ciudad, codigopin, telefono, notas
+        })
+
+        await nuevaCuenta.save();
+
+        res.status(201).json({
+            success: true,
+            data: nuevaCuenta
+        })
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
