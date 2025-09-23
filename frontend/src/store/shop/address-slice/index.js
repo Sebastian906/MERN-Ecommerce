88e53fb3106f6ea23b524230306cfbca9b9fc691
Sebastian906 <elsebas1912@gmail.com>
2025-09-23 16:36:59 -0500

    Creción de slice para las cuentas

---
+++ frontend/src/store/shop/address-slice/index.js
@@ -0,0 +1,76 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    estaCargando: false,
+    addressList: [],
+}
+
+export const agregarNuevaCuenta = createAsyncThunk(
+    '/cuentas/agregar-cuenta', 
+    async(formData) => {
+        const response = await axios.post(
+            'http://localhost:5000/api/shop/cuenta/agregar', 
+            formData
+        );
+        return response.data;
+    }
+);
+
+export const listarTodosLasCuentas = createAsyncThunk(
+    '/cuentas/listar-cuentas', 
+    async(usuarioId) => {
+        const response = await axios.get(
+            `http://localhost:5000/api/shop/cuenta/listar/${usuarioId}`, 
+        );
+        return response.data;
+    }
+);
+
+export const editarCuenta = createAsyncThunk(
+    '/cuentas/editar-cuenta', 
+    async({usuarioId, cuentaId, formData}) => {
+        const response = await axios.put(
+            `http://localhost:5000/api/shop/cuenta/editar/${usuarioId}/${cuentaId}`,
+            formData
+        );
+        return response.data;
+    }
+);
+
+export const eliminarCuenta = createAsyncThunk(
+    '/cuentas/eliminar-cuenta', 
+    async({usuarioId, cuentaId}) => {
+        const response = await axios.delete(
+            `http://localhost:5000/api/shop/cuenta/eliminar/${usuarioId}/${cuentaId}`,
+        );
+        return response.data;
+    }
+);
+
+const addressSlice = createSlice({
+    name: 'address',
+    initialState,
+    reducers: {},
+    extraReducers : (builder) => {
+        builder.addCase(agregarNuevaCuenta.pending, (state) => {
+            state.estaCargando = true;
+        }).addCase(agregarNuevaCuenta.fulfilled, (state, action) => {
+            state.estaCargando = false;
+            state.addressList = action.payload.data;
+        }).addCase(agregarNuevaCuenta.rejected, (state) => {
+            state.estaCargando = false;
+            state.addressList = [];
+        }).addCase(listarTodosLasCuentas.pending, (state) => {
+            state.estaCargando = true;
+        }).addCase(listarTodosLasCuentas.fulfilled, (state, action) => {
+            state.estaCargando = false;
+            state.addressList = action.payload.data;
+        }).addCase(listarTodosLasCuentas.rejected, (state) => {
+            state.estaCargando = false;
+            state.addressList = [];
+        });
+    }
+});
+
+export default addressSlice.reducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,6 +3,7 @@ import authReducer from './auth-slice'
 import adminProductsSlice from './admin/products-slice'
 import shoppingProductsSlice from './shop/products-slice'
 import shoppingCartSlice from './shop/cart-slice'
+import shoppingAddressSlice from './shop/address-slice'
 
 const store = configureStore({
     reducer : {
@@ -10,6 +11,7 @@ const store = configureStore({
         adminProductos: adminProductsSlice,
         tiendaProductos: shoppingProductsSlice,
         carritoProductos: shoppingCartSlice,
+        cuentaProductos: shoppingAddressSlice,
     },
 });
 
