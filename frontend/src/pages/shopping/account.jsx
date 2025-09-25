a9ea49685d19159fe66c12efd94ac5310b173e84
Sebastian906 <elsebas1912@gmail.com>
2025-09-25 12:20:34 -0500

    Creación inicial de interfaz para las cuentas e instalación de componente Tabs

--- frontend/src/pages/shopping/account.jsx
+++ frontend/src/pages/shopping/account.jsx
@@ -1,7 +1,38 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useState } from 'react';
+import accImg from '../../assets/account.jpg'
+
 function ShoppingAccount() {
+    const [activeTab, setActiveTab] = useState('ordenes');
     return ( 
-        <div>
-            Shopping View Account
+        <div className="flex flex-col bg-pink-100">
+            <div className="relative h-[300px] w-full">
+                <img
+                    style={{aspectRatio: '1600/300', objectFit: 'cover'}}
+                    src={accImg}
+                    className='h-full w-full object-cover object-center'
+                />
+            </div>
+            <div className='container mx-auto grid grid-cols-1 gap-8 py-8'>
+                <div className='flex flex-col rounded-lg border bg-background p-6 shadow-sm'>
+                    <Tabs value={activeTab} onValueChange={setActiveTab}>
+                        <TabsList>
+                            <TabsTrigger value="ordenes" className={`px-4 py-2 rounded-l-lg border ${activeTab === 'ordenes' ? '!bg-pink-100 !text-black' : '!bg-pink-100 !text-black opacity-50'}`}>
+                                Ordenes
+                            </TabsTrigger>
+                            <TabsTrigger value="cuenta" className={`px-4 py-2 rounded-r-lg border ${activeTab === 'cuenta' ? '!bg-pink-100 !text-black' : '!bg-pink-100 !text-black opacity-50'}`}>
+                                Cuenta
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="ordenes">
+                            Hola
+                        </TabsContent>
+                        <TabsContent value="cuenta">
+                            Mundo
+                        </TabsContent>
+                    </Tabs>
+                </div>
+            </div>
         </div>
     );
 }
