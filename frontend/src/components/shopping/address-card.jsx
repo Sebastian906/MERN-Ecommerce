ae49bb6c776fa8127981b71652292afd4307d203
Sebastian906 <elsebas1912@gmail.com>
2025-09-27 22:51:01 -0500

    Se agregan funciones para editar y borrar una cuenta dentro del panel de usuario

--- frontend/src/components/shopping/address-card.jsx
+++ frontend/src/components/shopping/address-card.jsx
@@ -1,16 +1,26 @@
-import { Card, CardContent } from "../ui/card";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter } from "../ui/card";
 import { Label } from "../ui/label";
 
-function AddressCard({ addressInfo }) {
+function AddressCard({ 
+    addressInfo, 
+    handleDeleteAddress, 
+    handleEditAddress 
+}) {
+
     return ( 
         <Card>
             <CardContent className="flex flex-col p-4 gap-2 min-w-[250px] max-w-[350px] bg-transparent">
-                <Label className="mb-1"><span className="font-semibold">Address:</span> {addressInfo?.cuenta}</Label>
-                <Label className="mb-1"><span className="font-semibold">City:</span> {addressInfo?.ciudad}</Label>
-                <Label className="mb-1"><span className="font-semibold">Pincode:</span> {addressInfo?.codigopin}</Label>
-                <Label className="mb-1"><span className="font-semibold">Phone:</span> {addressInfo?.telefono}</Label>
-                <Label><span className="font-semibold">Notes:</span> {addressInfo?.notas}</Label>
+                <Label className="mb-1"><span className="font-semibold">Cuenta:</span> {addressInfo?.cuenta}</Label>
+                <Label className="mb-1"><span className="font-semibold">Ciudad:</span> {addressInfo?.ciudad}</Label>
+                <Label className="mb-1"><span className="font-semibold">Código Pin:</span> {addressInfo?.codigopin}</Label>
+                <Label className="mb-1"><span className="font-semibold">Teléfono:</span> {addressInfo?.telefono}</Label>
+                <Label><span className="font-semibold">Notas:</span> {addressInfo?.notas}</Label>
             </CardContent>
+            <CardFooter className="p-3 flex justify-between">
+                <Button onClick={() => handleEditAddress(addressInfo)} className="!bg-purple-500 hover:!bg-purple-600 !text-neutral-100">Editar</Button>
+                <Button onClick={() => handleDeleteAddress(addressInfo)} className="!bg-pink-500 hover:!bg-pink-600 !text-neutral-100">Eliminar</Button>
+            </CardFooter>
         </Card>
     );
 }
--- frontend/src/components/shopping/address.jsx
+++ frontend/src/components/shopping/address.jsx
@@ -3,8 +3,9 @@ import CommonForm from "../common/form";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { addressFormControls } from "@/config";
 import { useDispatch, useSelector } from "react-redux";
-import { agregarNuevaCuenta, listarTodosLasCuentas } from "@/store/shop/address-slice";
+import { agregarNuevaCuenta, editarCuenta, eliminarCuenta, listarTodosLasCuentas } from "@/store/shop/address-slice";
 import AddressCard from "./address-card";
+import { toast } from "sonner";
 
 const initialAddressFormData = {
     cuenta: '',
@@ -17,21 +18,66 @@ const initialAddressFormData = {
 function Address() {
 
     const [formData, setFormData] = useState(initialAddressFormData)
+    const [currentEditedId, setCurrentEditedId] = useState(null);
     const ejecucion = useDispatch();
     const { usuario } = useSelector(state => state.auth)
     const { addressList } = useSelector(state => state.cuentaProductos)
 
     function handleManageAddress(event) {
         event.preventDefault();
-        ejecucion(agregarNuevaCuenta({
+
+        if (addressList.length >= 3 && currentEditedId === null) {
+            setFormData(initialAddressFormData)
+            toast.error("Se pueden añadir hasta 3 cuentas");
+            return;
+        }
+
+        currentEditedId !== null ?
+            ejecucion(
+                editarCuenta({
+                    usuarioId: usuario?.id,
+                    cuentaId: currentEditedId,
+                    formData
+                })).then((data) => {
+                    if (data?.payload?.success) {
+                        ejecucion(listarTodosLasCuentas(usuario?.id));
+                        setCurrentEditedId(null);
+                        setFormData(initialAddressFormData)
+                        toast.info("Datos de dirección actualizados.")
+                    }
+                }) :
+            ejecucion(agregarNuevaCuenta({
+                ...formData,
+                usuarioId: usuario?.id
+            })).then(data => {
+                if (data?.payload?.success) {
+                    ejecucion(listarTodosLasCuentas(usuario?.id));
+                    setFormData(initialAddressFormData);
+                    toast.success("Datos de dirección actualizados.")
+                }
+            });
+    }
+
+    function handleEditAddress(getCurrentAddress) {
+        setCurrentEditedId(getCurrentAddress?._id)
+        setFormData({
             ...formData,
-            usuarioId: usuario?.id
-        })).then(data => {
+            cuenta: getCurrentAddress?.cuenta,
+            ciudad: getCurrentAddress?.ciudad,
+            telefono: getCurrentAddress?.telefono,
+            codigopin: getCurrentAddress?.codigopin,
+            notas: getCurrentAddress?.notas
+        })
+    }
+
+    function handleDeleteAddress(getCurrentAddress) {
+        console.log(getCurrentAddress);
+        ejecucion(eliminarCuenta({ usuarioId: usuario?.id, cuentaId: getCurrentAddress._id })).then(data => {
             if (data?.payload?.success) {
-                ejecucion(listarTodosLasCuentas(usuario?.id));
-                setFormData(initialAddressFormData);
+                ejecucion(listarTodosLasCuentas(usuario?.id))
+                toast.warning("Los datos de la cuenta han sido borrados.")
             }
-        });
+        })
     }
 
     function isFormValid() {
@@ -50,14 +96,23 @@ function Address() {
         <>
             <div className="mb-5 p-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {
-                    addressList && addressList.length > 0 ?
-                        addressList.map(singleAddressItem => <AddressCard key={singleAddressItem._id} addressInfo={singleAddressItem} />) : null
+                    addressList && addressList.length > 0
+                        ? addressList.map(singleAddressItem => (
+                            <AddressCard
+                                handleDeleteAddress={handleDeleteAddress}
+                                key={singleAddressItem._id}
+                                addressInfo={singleAddressItem}
+                                handleEditAddress={handleEditAddress}
+                            />
+                        )) : null
                 }
             </div>
             <Card>
                 <CardHeader>
                     <CardTitle>
-                        Agregar Nueva Cuenta
+                        {
+                            currentEditedId !== null ? 'Editar Cuenta' : 'Agregar Nueva Cuenta'
+                        }
                     </CardTitle>
                 </CardHeader>
                 <CardContent className="space-y-3">
@@ -65,7 +120,7 @@ function Address() {
                         formControls={addressFormControls}
                         formData={formData}
                         setFormData={setFormData}
-                        buttonText={'Agregar'}
+                        buttonText={currentEditedId !== null ? 'Editar' : 'Agregar'}
                         onSubmit={handleManageAddress}
                         isBtnDisabled={!isFormValid()}
                     />
